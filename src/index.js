21b22b2ad626402ead41d456ff74a1b608e41c43
Jhcki222 <iqbox2@naver.com>
2023-08-24 14:59:43 +0900

    Auto stash before merge of "develop" and "origin/develop"

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Intro />
   </React.StrictMode>
 );
 
