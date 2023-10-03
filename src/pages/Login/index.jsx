5919646b6da86557bb0a883b72b81a22532385f6
Cllaude99 <xodbsrla1025@naver.com>
2023-10-04 00:21:07 +0900

    Feat: "Kakao Login"

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,46 +1,43 @@
-import React from 'react'
-import styled from 'styled-components'
-import LoginImg from '../../assets/imgs/login_bg.png'
-import {ReactComponent as LoginChar} from '../../assets/imgs/login_char.svg'
-import {ReactComponent as LoginStartMsg} from '../../assets/imgs/login_start_msg.svg'
-import KakaoImg from '../../assets/imgs/kakao_login.png'
-import NaverImg from '../../assets/imgs/naver_login.png'
+import React from "react";
+import styled from "styled-components";
+import LoginImg from "../../assets/imgs/login_bg.png";
+import { ReactComponent as LoginChar } from "../../assets/imgs/login_char.svg";
+import { ReactComponent as LoginStartMsg } from "../../assets/imgs/login_start_msg.svg";
+import KakaoImg from "../../assets/imgs/kakao_login.png";
+import NaverImg from "../../assets/imgs/naver_login.png";
 
 export default function Login() {
-
   const handleKakaoLogin = () => {
     // kakao login api 연결
-  }
+  };
 
   const handleNaverLogin = () => {
     // naver login api 연결
-  }
+  };
 
   return (
     <>
-      <LoginContainer >
-        <LoginBg/>
-        <Overlay/>
+      <LoginContainer>
+        <LoginBg />
+        <Overlay />
         <LoginWrapper>
           <p>로그인</p>
-          <LoginChar/>
+          <LoginChar />
           <LoginBtn>
-            <LoginStartMsg/>
+            <LoginStartMsg />
             <SocialLogin onClick={handleKakaoLogin} socialImg={KakaoImg} />
             <SocialLogin onClick={handleNaverLogin} socialImg={NaverImg} />
           </LoginBtn>
         </LoginWrapper>
-        
       </LoginContainer>
     </>
-  )
+  );
 }
 
 const LoginContainer = styled.div`
   position: relative;
   height: calc(100vh - 10rem);
-
-`
+`;
 
 const LoginBg = styled.div`
   background: no-repeat;
@@ -48,15 +45,15 @@ const LoginBg = styled.div`
   background-image: url(${LoginImg});
   width: 100%;
   height: 100%;
-`
+`;
 
 const Overlay = styled.div`
   position: absolute;
-  background-color: rgba(47, 23, 0, 0.50);
+  background-color: rgba(47, 23, 0, 0.5);
   width: 100%;
   height: 100%;
-  top:0;
-`
+  top: 0;
+`;
 
 const LoginWrapper = styled.div`
   position: absolute;
@@ -68,16 +65,14 @@ const LoginWrapper = styled.div`
   font-weight: 900;
   text-align: center;
 
-  p{
+  p {
     margin-bottom: 0.8rem;
   }
 
-  
-  svg{
+  svg {
     margin: 0 auto;
   }
-
-`
+`;
 
 const LoginBtn = styled.div`
   display: flex;
@@ -85,13 +80,13 @@ const LoginBtn = styled.div`
   gap: 1rem;
   align-items: center;
   margin-top: 2.6rem;
-`
+`;
 
-
-
-const SocialLogin = styled.img.attrs(props => ({
-  src: props.socialImg
+const SocialLogin = styled.img.attrs((props) => ({
+  src: props.socialImg,
 }))`
   width: 25rem;
   height: 3.75rem;
-`
+  cursor: pointer;
+  margin-bottom: 10px;
+`;
