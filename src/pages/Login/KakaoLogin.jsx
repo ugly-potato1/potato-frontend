58f7d6c8b4636a45c285fdd7a9c5ecd86f843b77
TaeYoon Kim <xodbsrla1025@naver.com>
2023-10-07 14:16:02 +0900

    Security:"add Kakao clientId in env file 수정"
    env파일 변수 앞에 'REAC_APP' 추가

--- src/pages/Login/KakaoLogin.jsx
+++ src/pages/Login/KakaoLogin.jsx
@@ -12,8 +12,8 @@ const Loading = styled.div`
 
 const KakaoLogin = () => {
   const navigate = useNavigate();
-  const client_id = process.env.KAKAO_REST_API_KEY;
-  const redirect_uri = process.env.KAKAO_REDIRECT_URI;
+  const client_id = process.env.REACT_APP_KAKAO_REST_API_KEY;
+  const redirect_uri = process.env.REACT_APP_KAKAO_REDIRECT_URI;
   const code = useSearchParams()[0].get("code");
 
   const getKakaoToken = async () => {
