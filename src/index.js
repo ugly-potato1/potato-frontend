a398106f23941b4c4ef38e6a4d531d7f82ec7f53
TaeYoon Kim <xodbsrla1025@naver.com>
2024-01-24 12:36:57 +0900

    Chore:"우편번호 모듈 설치"

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Main from './pages/Main';
 import Login from './pages/Login';
 import Intro from './pages/Intro';
@@ -68,10 +69,13 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <RecoilRoot>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </RecoilRoot>
 );
 
